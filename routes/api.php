b076512be628168e0a5c4a94558649c5d040f074
Tung <devtung27101992@gmail.com>
2025-08-30 00:41:48 +0700

    update code

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\PurchaseController;
 
 Route::post('orders/update-field/{id}', [OrderController::class, 'updateField'])
     ->name('api.orders.update-field');
 
+Route::post('purchases/update-field/{id}', [PurchaseController::class, 'updateField'])
+    ->name('api.purchases.update-field');
