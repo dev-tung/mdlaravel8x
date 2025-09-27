c56d9e498aa2c37407dfc94491c0f84b5d70c157
Tung <devtung27101992@gmail.com>
2025-09-27 23:24:35 +0700

    UpdateCode

--- app/Http/Controllers/Api/SupplierController.php
+++ app/Http/Controllers/Api/SupplierController.php
@@ -8,23 +8,23 @@ use Illuminate\Http\Request;
 
 class SupplierController extends Controller
 {
-    protected $repo;
+    protected $repository;
 
-    public function __construct(SupplierRepository $repo)
+    public function __construct(SupplierRepository $repository)
     {
-        $this->repo = $repo;
+        $this->repository = $repository;
     }
 
     public function index()
     {
-        return response()->json($this->repo->all());
+        return response()->json($this->repository->all());
     }
 
     public function updateField(Request $request, $id)
     {
         $field = $request->input('field');
         $value = $request->input('value');
-        $success = $this->repo->updateField($id, $field, $value);
+        $success = $this->repository->updateField($id, $field, $value);
         return response()->json(['success' => $success]);
     }
 }
